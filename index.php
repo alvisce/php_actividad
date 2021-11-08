092c988e22286fbe3d1ef1ac5fb65d483dbbb675
Cesar <notengo@mail.com>
2021-11-08 01:15:45 -0300

    Desactiva el cache

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $datos = array(
 ?>
 <html>
 <head>
-	<title>Modulo PHP - Unidad 1</title>
+	<title>Modulo PHP - Unidad 2</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -49,7 +49,7 @@ $datos = array(
 		</ul>
 	</main>
 	<footer class="container d-flex flex-column flex-sm-row alert-dark my-1 py-1">
-		<span class="mx-auto">PHP FullStack</span>
+		<span class="mx-auto">FullStack PHP</span>
 		<span class="fw-bold mx-auto"><?=$datos['nombre'] . ' ' . $datos['apellido']?></span>
 		<span class="mx-auto">Comisi&oacute;n 2162</span>
 	</footer>
